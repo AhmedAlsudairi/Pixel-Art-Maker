98392b234fbbdff21ab34349a0b69402319d38e3
Ahmed Alsudairi <a7madalsudairi@gmail.com>
2020-08-08 05:44:08 +0300

    change the structure of js file

--- designs.js
+++ designs.js
@@ -1,26 +1,27 @@
 // Select color input
 // Select size input
-    let height = document.getElementById('inputHeight').value;
-    let width = document.getElementById('inputWidth').value;
-    let color = document.getElementById('colorPicker').value;
+
+
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+    let height = document.getElementById('inputHeight').value;
+    let width = document.getElementById('inputWidth').value;
+    let color = document.getElementById('colorPicker').value;
     const table = document.getElementById('pixelCanvas');
 
-    for(let i = 0; i<width; i++){
+    for (let i = 0; i < height; i++) {
         let row = document.createElement('tr');
-        for(let j = 0; j<height; i++){
+        for (let j = 0; j < width; j++) {
             let cell = document.createElement('td');
             row.appendChild(cell);
+
+            cell.addEventListener('click', (event) => { event.target.setAttribute('style', 'background-color:' + color) });
         }
         table.appendChild(row);
     }
-    document.addEventListener('mousemove',)
-  }
-  
-  function processMouseMove(e) {
-      console.log(e.target);
-    e.target.setAttribute('style','background-color: '+color)
-  }
-  document.getElementById('sizePicker').addEventListener('submit',makeGrid);
+}
+document.getElementById('sizePicker').addEventListener('submit', (event) => {
+    event.preventDefault();
+    makeGrid()
+});
